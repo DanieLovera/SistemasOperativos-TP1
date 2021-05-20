6ca0b5694d1b8b1aeabb7a224fb7baa1c0087758
Daniel Lovera <daanilo2497@gmail.com>
2021-05-20 19:06:36 -0300

    Refactorizacion de la funcion read_exe_directory
    Se cambio la funcion por read_directory para que pueda ser reusada por
    cualquier llamado que se le tenga que hacer al usuario para que defina
    la ruta de un directorio.
    
    La funcion make_directory seguramente no podra ser reusada de esta forma
    pero por ahora quedo como una sola para poder testear que se creaban los
    directorios correctamente.

--- Grupo4/sisop/sotp1.sh
+++ Grupo4/sisop/sotp1.sh
@@ -1,48 +1,59 @@
 #!/bin/bash
 
 conf_file_path="./sotp1.conf"
-dir_exe="../bin"
+exe_dir=$(pwd | sed 's-/sisop$-/bin-')
+sys_tables_dir=$(pwd | sed 's-/sisop$-/master-')
+news_input_dir=$(pwd | sed 's-/sisop$-/ENTRADATP-')
+rejected_files_dir=$(pwd | sed 's-/sisop$-/rechazos-')
+lots_dir=$(pwd | sed 's-/sisop$-/lotes-')
+results_dir=$(pwd | sed 's-/sisop$-/SALIDATP-')
 
-
-# Lee el directorio de entrada.
+# Lee algun directorio de entrada del usuario.
 # El directorio ingresado por el usuario debe encontrarse en la ruta
 # $(GRUPO4)/, las verificaciones de existencias de archivos se realizan
 # sobre el directorio padre de $(GRUPO4) para validar la no inclusion
 # del mismo archivo Grupo4. Por lo tanto como hipotesis, no se pueden
 # definir rutas de archivos como nombres que esten fuera de $(GRUPO4)/
-function read_exe_directory() {
-	echo "- Defina el nombre del directorio de ejecutables o presione enter para"
-	read -p "  continuar [directorio por defecto ${dir_exe}]: " dir_exe_tmp
+# @param $1: mensaje que se quiere mostrar en pantalla (como contexto
+# del directorio)
+# @param $2: ruta del directorio por defecto
+# @return tmp_dir: se retorna como variable global necesaria para 
+# devolver un string.
+function read_directory() {
+	echo "- Defina el nombre del $1 o presione enter para"
+	read -p "  continuar [directorio por defecto $2]: " tmp_dir
 
-	if [ -z ${dir_exe_tmp} ]
+	if [ -z ${tmp_dir} ]
 	then
-		dir_exe_tmp=${dir_exe}
+		tmp_dir=$2
 	fi
 
-	local found=$(find ../.. -type d -name ${dir_exe_tmp##*/})
+	local found=$(find ../.. -type d -name ${tmp_dir##*/})
 	while [ ! -z ${found} ] 
 	do
 		echo -e "\nNombre invalido, el directorio ya existe."
-		echo "- Defina el nombre del directorio de ejecutables o presione enter para"
-		read -p "  continuar [directorio por defecto ${dir_exe}]: " dir_exe_tmp
+		echo "- Defina el nombre del $1 o presione enter para"
+		read -p "  continuar [directorio por defecto $2]: " tmp_dir
 
-		if [ -z ${dir_exe_tmp} ]
+		if [ -z ${tmp_dir} ]
 		then
-			dir_exe_tmp=${dir_exe}
+			tmp_dir=$2
 		fi
 
-		found=$(find ../.. -type d -name ${dir_exe_tmp##*/})
+		found=$(find ../.. -type d -name ${tmp_dir##*/})
 	done
 
-	mkdir ${dir_exe_tmp}
-	dir_exe=$(find "$(cd ../..; pwd)" -type d -name ${dir_exe_tmp##*/})
-	rm -r ${dir_exe_tmp}
+	mkdir ${tmp_dir}
+	tmp_dir=$(find "$(cd ../..; pwd)" -type d -name ${tmp_dir##*/})
+	rm -r ${tmp_dir}
 }
 
 # Crea el directorio de ejecucion
-function make_exe_directory() {
-	mkdir $1
-	echo -e "\nDirectorio de ejecutables creado en: $1"
+# @param $1: mensaje del directorio creado.
+# @param $2: ruta de archivo que se va a crear.
+function make_directory() {
+	mkdir $2
+	echo -e "$1 creado en: $2"
 }
 
 # Instala el sistema haciendo uso de las demas funciones.
@@ -52,12 +63,30 @@ function install() {
 	if [ ! -f ${conf_file_path} ]
 	then
 		echo -e "Comenzando instalacion del sistema...\n"
-		read_exe_directory
+		
+		read_directory "directorio de ejecutables" ${exe_dir}
+		exe_dir=${tmp_dir}
+		read_directory "directorio de tablas del sistema" ${sys_tables_dir}
+		sys_tables_dir=${tmp_dir}
+		read_directory "directorio de novedades" ${news_input_dir}
+		news_input_dir=${tmp_dir}
+		read_directory "directorio de archivos rechazados" ${rejected_files_dir}
+		rejected_files_dir=${tmp_dir}
+		read_directory "directorio de lotes procesados" ${lots_dir}
+		lots_dir=${tmp_dir}
+		read_directory "directorio de resultados" ${results_dir}
+		results_dir=${tmp_dir}
 
 		#echo "Creando archivo de configuracion ${conf_file_path#./}..."
 		##touch ${conf_file_path}
 		#echo "${conf_file_path#./} correctamente instalado."
-		make_exe_directory ${dir_exe}
+		echo " "
+		make_directory "Directorio de ejecutables" ${exe_dir}
+		make_directory "Directorio de tablas del sistema" ${sys_tables_dir}
+		make_directory "Directorio de novedades" ${news_input_dir}
+		make_directory "Directorio de archivos rechazados" ${rejected_files_dir}
+		make_directory "Directorio de lotes procesados" ${lots_dir}
+		make_directory "Directorio de resultados" ${results_dir}
 	else
 		echo "El sistema ya se encuentra instalado."
 	fi
